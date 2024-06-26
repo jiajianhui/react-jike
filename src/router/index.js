5790fb6b53941f9e7fa2ae41f5dc605028057e43
jiajianhui <jia15176168273@icloud.com>
2024-06-26 15:02:51 +0800

    根据token控制路由跳转

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 
 // 导入组件
+import { AuthRoute } from "@/components/AuthRoute";
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
+
 
 
 // 配置路由实例
@@ -10,7 +12,7 @@ import { createBrowserRouter } from "react-router-dom";
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Layout />
+        element: <AuthRoute> <Layout /> </AuthRoute>
     },
     {
         path: '/login',
