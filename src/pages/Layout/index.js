ff79f1ccbec092610ac9a94fe591f806026b7d8c
jiajianhui <jia15176168273@icloud.com>
2024-06-26 14:46:00 +0800

    axios请求头注入token

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,4 +1,13 @@
-function Layout() {
+import { request } from "@/utils"
+import { useEffect } from "react"
+
+const Layout = () => {
+
+    // 测试token是否注入
+    useEffect(() => {
+        request.get('/user/profile')
+    })
+
     return <div>Layout</div>
 }
 
