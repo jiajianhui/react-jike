5c49c0b558ab72214114db4c535b31ab6ddfa7d1
jiajianhui <jia15176168273@icloud.com>
2024-06-28 16:32:56 +0800

    echarts图表初始化

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,50 @@
+import * as echarts from 'echarts';
+import { useEffect } from 'react';
+
+
+
+
 const Home = () => {
-    return <div>Home</div>
+
+    // dom渲染完毕后，才进行图表的渲染
+    useEffect(() => {
+        // 1、获取渲染图表的dom节点
+        const chartDom = document.getElementById('main');
+
+        // 2、图表初始化生成图表实例对象
+        const myChart = echarts.init(chartDom);
+
+        // 3、准备图表参数
+        const option = {
+        xAxis: [
+            {
+            type: 'category',
+            data: ['Vue', 'Angular', 'React'],
+            axisTick: {
+                alignWithLabel: true
+            }
+            }
+        ],
+        yAxis: [
+            {
+            type: 'value'
+            }
+        ],
+        series: [
+            {
+            name: 'Direct',
+            type: 'bar',
+            barWidth: '60%',
+            data: [10, 40, 60]
+            }
+        ]
+        };
+
+        // 4、使用图表参数完成图表的渲染
+        option && myChart.setOption(option);
+    }, [])
+
+    return <div><div id='main' style={{width: '500px', height: '400px'}}></div></div>
 }
 
 export default Home
