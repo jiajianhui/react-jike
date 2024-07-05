b76f7795ba8ded36b283d41a89e7612f0217ce5f
jiajianhui <jia15176168273@icloud.com>
2024-07-05 09:32:57 +0800

    筛选功能实现

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -22,7 +22,7 @@ const Article = () => {
         1: <Tag color="warning">待审核</Tag>,
         2: <Tag color="green">审核通过</Tag>
     }
-
+    
     // 列数据
       const columns = [
     {
@@ -77,35 +77,50 @@ const Article = () => {
     }
   ]
 
-  const data = [
-      {
-          id: '8218',
-          comment_count: 0,
-          cover: {
-            images:[],
-          },
-          like_count: 0,
-          pubdate: '2019-03-11 09:00:00',
-          read_count: 2,
-          status: 2,
-          title: 'wkwebview离线化加载h5资源解决方案' 
-      }
-  ]
 
     // 频道列表数据-——调用函数，然后解构出需要的数据
     const {channelList} = useChannel()
 
-    // 获取文章列表、文章总数
+   
+
+    // 筛选功能
+    // 1、准备接口参数
+    const [reqData, setReqData] = useState({
+      status: '',
+      channel_id: '',
+      begin_pubdate: '',
+      end_pubdate: '',
+      page: 1,
+      per_page: 4
+    })
+    // 2、获取用户选择的表单数据（具体筛选项目）
+    const onFinish = (formData) => {
+      // console.log(formData);
+      // 3、把表单收集的数据放到参数中（不可变的方式）
+      setReqData({
+        ...reqData,  //展开运算符
+        status: formData.status,
+        channel_id: formData.channel_id,
+        begin_pubdate: formData.date[0].format('YYYY-MM-DD'),
+        end_pubdate: formData.date[1].format('YYYY-MM-DD')
+      })
+
+      // 4、重新拉取文章列表   渲染table（复用即可——reqData依赖项发生变化，useEffect函数就会再次执行）
+    }
+
+     // 获取文章列表、文章总数
     const [articleList, setArticleList] = useState([])
     const [articleCount, setArticleCount] = useState(0)
     useEffect(() => {
         async function getArticleList() {
-            const res = await getArticleAPI()
+            const res = await getArticleAPI(reqData)
             setArticleList(res.data.results)
             setArticleCount(res.data.total_count)
         }
         getArticleList()
-    }, [])
+    }, [reqData])
+
+
   return (
     <div>
       <Card
@@ -119,10 +134,12 @@ const Article = () => {
         }
         style={{ marginBottom: 20 }}
       >
-        <Form initialValues={{ status: null }}>
+        <Form initialValues={{ status: '' }} onFinish={onFinish}>
+
+        {/* 状态选择 */}
           <Form.Item label="状态" name="status">
             <Radio.Group>
-              <Radio value={null}>全部</Radio>
+              <Radio value={''}>全部</Radio>
               <Radio value={0}>草稿</Radio>
               <Radio value={1}>待审核</Radio>
               <Radio value={2}>审核通过</Radio>
@@ -130,10 +147,11 @@ const Article = () => {
             </Radio.Group>
           </Form.Item>
 
+        {/* 频道选择 */}
           <Form.Item label="频道" name="channel_id">
             <Select
               placeholder="请选择文章频道"
-              defaultValue="lucy"
+              // defaultValue="lucy"
               style={{ width: 120 }}
             >
                 {channelList.map(item => <Option key={item.id} value={item.id}>{item.name}</Option>)}
@@ -141,11 +159,13 @@ const Article = () => {
             </Select>
           </Form.Item>
 
+        {/* 日期选择 */}
           <Form.Item label="日期" name="date">
             {/* 传入locale属性 控制中文显示*/}
             <RangePicker locale={locale}></RangePicker>
           </Form.Item>
 
+        {/* 按钮 */}
           <Form.Item>
             <Button type="primary" htmlType="submit" style={{ marginLeft: 40 }}>
               筛选
