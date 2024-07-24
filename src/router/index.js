e4358b23f2c8bd51130d46a56966d8d08eb4237b
jiajianhui <jia15176168273@icloud.com>
2024-07-24 15:57:00 +0800

    完成路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,26 @@
 // 导入组件
 import { AuthRoute } from "@/components/AuthRoute";
 import Article from "@/pages/Article";
-import Home from "@/pages/Home";
-import Layout from "@/pages/Layout";
+// import Home from "@/pages/Home";
+// import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
-import Publish from "@/pages/Publish";
-
-
+// import Publish from "@/pages/Publish";
+import { lazy, Suspense } from "react";
 
 // 配置路由实例
 import { createBrowserRouter } from "react-router-dom";
+
+// 路由懒加载
+// 1、使用lazy函数对组件进行导入
+const Home = lazy(() => import('@/pages/Home'))
+const Layout = lazy(() => import('@/pages/Layout'))
+const Publish = lazy(() => import('@/pages/Publish'))
+
+// 2、
+
+
+
+
 
 const router = createBrowserRouter([
     {
@@ -21,15 +32,15 @@ const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <Home />
+                element: <Suspense fallback={'加载中'}> <Home /> </Suspense>
             },
             {
                 path: '/article',
-                element: <Article />
+                element: <Suspense fallback={'加载中'}> <Article /> </Suspense>
             },
             {
                 path: '/publish',
-                element: <Publish />
+                element: <Suspense fallback={'加载中'}> <Publish /> </Suspense>
             }
         ]
     },
